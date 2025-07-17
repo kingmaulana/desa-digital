926d3d592b9194bc05201d78e62c7b701325cf79
kingmaulana <akbarquipper68@gmail.com>
2025-07-17 07:25:16 +0700

    setting model and relation table

---
+++ desa-digital-api/app/Models/HeadOfFamily.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class HeadOfFamily extends Model
+{
+    use SoftDeletes, UUID;
+
+    protected $fillable = [
+        'user_id',
+        'profile_picture',
+        'identity_number',
+        'gender',
+        'date_of_birth',
+        'phone_number',
+        'occupation',
+        'marital_status',
+    ];
+
+    public function user() 
+    {
+        return $this->BelongsTo(User::class);
+    }
+
+    public function familyMember()
+    {
+        return $this->hasMany(FamilyMember::class);
+    }
+
+    public function socialAssistanceRecipients()
+    {
+        return $this->hasMany(SocialAssistanceRecipient::class);
+    }
+
+    public function eventParticipants()
+    {
+        return $this->hasMany(EventParticipant::class);
+    }
+}
